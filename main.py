46ecb4303633c0a4f65d340717288a9c35202673
nicosantome <nicosantome@gmail.com>
2024-03-26 00:17:21 +0100

    SQL DB, create record and get record to display in index.html working

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
 
 '''
 Red underlines? Install the required packages first: 
@@ -13,29 +16,57 @@ pip3 install -r requirements.txt
 This will install the packages from requirements.txt for this project.
 '''
 
+
 app = Flask(__name__)
 
-all_books = []
+class Base(DeclarativeBase):
+
+  pass
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
+
+db = SQLAlchemy(model_class=Base)
+
+db.init_app(app)
+
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+
+# Create table schema in the database. Requires application context.
+with app.app_context():
+    db.create_all()
+
+# CREATE RECORD
+# with app.app_context():
+#     new_book = Book(title="Tetor", author="J. K. Rowling", rating=9.3)
+#     db.session.add(new_book)
+#     db.session.commit()
 
 
 @app.route('/')
 def home():
+    with app.app_context():
+        result = db.session.execute(db.select(Book).order_by(Book.title))
+        all_books = result.scalars().all()  # Collect all books in a list
+        print(all_books)
     return render_template('index.html', books=all_books)
 
 
 @app.route("/add", methods=['GET', 'POST'])
 def add():
     if request.method == "POST":
-        new_book = {
-            'title': request.form['title'],
-            'author': request.form['author'],
-            'rating': request.form['rating']
-        }
-        all_books.append(new_book)
-        print(all_books)
+
+        with app.app_context():
+            new_book = Book(title=request.form['title'], author=request.form['author'], rating=request.form['rating'])
+            db.session.add(new_book)
+            db.session.commit()
         return redirect(url_for('home'))
     return render_template('add.html')
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=False)
 
